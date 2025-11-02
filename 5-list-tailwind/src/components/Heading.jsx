7e6aaf14db5f6bc7de64c16f741b892601314ed8
vanikrishna-dev <vanikrishna02@gmail.com>
2025-11-02 22:44:43 +0530

    twelfth commit

--- 5-list-tailwind/src/components/Heading.jsx
+++ 5-list-tailwind/src/components/Heading.jsx
@@ -1,5 +1,7 @@
 const Heading = () => {
-    return <h1 className="text-5xl text-center font-extrabold font-serif underline uppercase mt-5">Student List</h1>
+    return <h1 className=" bg-zinc-200 p-8 text-5xl rounded-3xl text-center font-extrabold font-serif underline uppercase mt-5">
+        Student List
+        </h1>
 }
 
 export default Heading;
--- 5-list-tailwind/src/components/List.jsx
+++ 5-list-tailwind/src/components/List.jsx
@@ -1,6 +1,6 @@
 const List = ({list}) => {
     return (
-        <ol className="ml-8 list-decimal">
+        <ol className=" bg-red-300 rounded-3xl p-8 font-bold list-decimal">
             {list.map(item => <li>{item}</li>)}
         </ol>
     )
