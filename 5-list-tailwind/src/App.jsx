6a0103031f3d46d4b03712b97e7e3c1c790d4c39
vanikrishna-dev <vanikrishna02@gmail.com>
2025-11-05 23:51:32 +0530

    seventeenth commit

--- 5-list-tailwind/src/App.jsx
+++ 5-list-tailwind/src/App.jsx
@@ -5,15 +5,19 @@ import Subheading from "./components/Subheading";
  function App() {
   const studentArr = ["Abhishek", "Kajol", "Ravi", "Ajay", "Sanjay", "Shashi", "Sanjeev"]
   let studentArr2;
+  const studentArr3 = ["Arushi", "Saniya", "Vipin", "Kartik", "Anmol"]
   return (
     <>
       <Heading></Heading>
       <br />
       <Subheading batch="1"></Subheading>
-      <br />
       <List list={studentArr}></List>
       <br />
+      <Subheading batch="2"></Subheading>
       <List list={studentArr2}></List>
+      <br />
+      <Subheading batch="3"></Subheading>
+      <List list={studentArr3}></List>
     </>
   )
 } 
