4a0e9cf7cbd64753ae7794ff45102f32bb01a4a9
vanikrishna-dev <vanikrishna02@gmail.com>
2025-11-06 00:35:57 +0530

    ninteenth commit

--- 5-list-tailwind/src/App.jsx
+++ 5-list-tailwind/src/App.jsx
@@ -4,7 +4,7 @@ import Subheading from "./components/Subheading";
 
  function App() {
   const studentArr = ["Abhishek", "Kajol", "Ravi", "Ajay", "Sanjay", "Shashi", "Sanjeev"]
-  let studentArr2;
+  let studentArr2 = ["Ravi", "Amit"];
   const studentArr3 = ["Arushi", "Saniya", "Vipin", "Kartik", "Anmol"]
   return (
     <>
@@ -17,7 +17,7 @@ import Subheading from "./components/Subheading";
       <List list={studentArr2}></List>
       <br />
       <Subheading batch="3"></Subheading>
-      <List list={studentArr3}></List>
+      <List list={studentArr3}></List> 
     </>
   )
 } 
--- 5-list-tailwind/src/components/List.jsx
+++ 5-list-tailwind/src/components/List.jsx
@@ -1,13 +1,14 @@
 const List = ({list}) => {
-    if (!list || list.length === 0){
-        return <h2 className=" bg-red-300 rounded-3xl p-8 font-bold text-center">No students are enrolled.</h2>
-    }
-    
+
     return (
-        <ol className=" bg-red-300 rounded-3xl p-8 font-bold list-decimal">
-            {list.map(item => <li>{item}</li>)}
-        </ol> 
-    )
-}
+        <>
+            {!list || list.length === 0 ? <h2>No students are enrolled.</h2>
+                                        : <h2>Here are the students:</h2>}
+            <ol className=" bg-red-300 rounded-3xl p-8 font-bold list-decimal">
+                {list.map(item => <li>{item}</li>)}
+            </ol>
+        </> 
+    );
+};
 
 export default List;
