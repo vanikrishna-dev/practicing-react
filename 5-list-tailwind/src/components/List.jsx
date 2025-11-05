35c8c804ef1d7474501109afa8d97e6d18438233
vanikrishna-dev <vanikrishna02@gmail.com>
2025-11-06 00:39:13 +0530

    twentieth commit

--- 5-list-tailwind/src/components/List.jsx
+++ 5-list-tailwind/src/components/List.jsx
@@ -3,7 +3,7 @@ const List = ({list}) => {
     return (
         <>
             {!list || list.length === 0 ? <h2>No students are enrolled.</h2>
-                                        : <h2>Here are the students:</h2>}
+                                        : <h2 className="bg-gray-700 rounded-3xl p-1 text-slate-50 font-medium">Here are the students:</h2>}
             <ol className=" bg-red-300 rounded-3xl p-8 font-bold list-decimal">
                 {list.map(item => <li>{item}</li>)}
             </ol>
