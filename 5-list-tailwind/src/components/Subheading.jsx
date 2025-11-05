02f0787169ea22381063ba0a34506cc2043ced73
vanikrishna-dev <vanikrishna02@gmail.com>
2025-11-05 23:47:27 +0530

    sixteenth commit

--- 5-list-tailwind/src/components/Subheading.jsx
+++ 5-list-tailwind/src/components/Subheading.jsx
@@ -1,5 +1,5 @@
 const Subheading = ({batch}) => {
-    return <h2>Batch {batch}</h2>;
+    return <h2 className="text-2xl font-bold font-serif bg-black text-white p-5 rounded-3xl">Batch {batch}</h2>;
 };
 
 export default Subheading;
