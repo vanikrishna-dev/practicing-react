e09816a1183f7e2db123dc56abf80340bc53d2b1
vanikrishna-dev <vanikrishna02@gmail.com>
2025-11-05 23:33:05 +0530

    fourteenth commit

--- 5-list-tailwind/src/App.jsx
+++ 5-list-tailwind/src/App.jsx
@@ -2,13 +2,15 @@ import Heading from "./components/Heading"
 import List from "./components/List"
 
  function App() {
-  // const studentArr = ["Abhishek", "Kajol", "Ravi", "Ajay", "Sanjay", "Shashi", "Sanjeev"]
-  let studentArr;
+  const studentArr = ["Abhishek", "Kajol", "Ravi", "Ajay", "Sanjay", "Shashi", "Sanjeev"]
+  let studentArr2;
   return (
     <>
       <Heading></Heading>
       <br />
       <List list={studentArr}></List>
+      <br />
+      <List list={studentArr2}></List>
     </>
   )
 } 
