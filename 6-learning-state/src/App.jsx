a27cdd3f3ad6bc66dfdcb5f466e7dd7a69624189
vanikrishna-dev <vanikrishna02@gmail.com>
2025-11-09 23:31:46 +0530

    23rd commit

--- 6-learning-state/src/App.jsx
+++ 6-learning-state/src/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import List from "./components/List"
 
  function App() {
-  const studentArr = ["Abhishek", "Kajol", "Ravi", "Ajay", "Sanjay", "Shashi", "Sanjeev"]
   let studentArr2;
   const studentArr3 = ["Arushi", "Saniya", "Vipin", "Kartik", "Anmol"]
   
+  const [studentArr, setStudentArr] = useState(["Abhishek", "Kajol", "Ravi", "Ajay", "Sanjay", "Shashi", "Sanjeev"]);
+
   const onChangeHandler = (event) => {
     if(event.key === "Enter"){
     console.log(event.target.value);
+    const newArr = [...studentArr, event.target.value];
+    event.target.value="";
+    setStudentArr(newArr);
     studentArr.push(event.target.value);
     console.log(studentArr);
     }
@@ -18,11 +23,12 @@ import List from "./components/List"
       <h1>Learners of MERN Stack</h1>
       <br />
       <List list={studentArr}></List>
+      <input type="text" placeholder="New Student name" onKeyDown={onChangeHandler}/>
       <br />
       <List list={studentArr2}></List>
       <br />
       <List list={studentArr3}></List> 
-      <input type="text" placeholder="New Student name" onKeyDown={onChangeHandler}/>
+      
     </>
   )
 }  
