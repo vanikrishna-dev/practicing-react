3e93c4a356f845f1975c5bcfff53b313e0b0ed28
vanikrishna-dev <vanikrishna02@gmail.com>
2025-11-13 14:31:50 +0530

    29th commit

---
+++ 9-Context-API/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center text-red-700 font-bold underline">
+        Welcome to Theme Changing App!
+      </h1>
+      <button className="m-3 border-gray-950 p-2 text-xl bg-slate-400 rounded" >Change Theme </button>
+    </>
+  )
+}
+
+export default App
