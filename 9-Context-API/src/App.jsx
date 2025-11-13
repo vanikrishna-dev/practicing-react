f8a0bd619e78d6896b1005a609c46eafa3b39aa9
vanikrishna-dev <vanikrishna02@gmail.com>
2025-11-14 00:45:06 +0530

    33rd commit

--- 9-Context-API/src/App.jsx
+++ 9-Context-API/src/App.jsx
@@ -1,23 +1,19 @@
-import { useState } from "react"
-import Button from "./components/Button"
-import Heading from "./components/Heading"
-import ThemeContext from "./store/ThemeContext"
+import { useContext } from "react";
+import Button from "./components/Button";
+import Heading from "./components/Heading";
+import ThemeContext from "./store/ThemeContext";
+
 
 function App() {
-  const [theme, setTheme] = useState('light');
 
-  const changeTheme = () => {
-    setTheme(currentTheme => currentTheme === 'light' ? 'dark' : 'light');
-  }
+  const {theme} = useContext(ThemeContext);
 
   return (
-      <ThemeContext.Provider value={{theme, changeTheme}}>
       <div className={`h-screen ${theme === 'light' ? " bg-pink-100" : "bg-blue-950"}`}>
         <Heading />
         <br />
         <Button />
       </div>
-      </ThemeContext.Provider>
   )
 }
 
--- 9-Context-API/src/store/ThemeContext.jsx
+++ 9-Context-API/src/store/ThemeContext.jsx
@@ -1,5 +1,19 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const ThemeContext = createContext();
+
+export const ThemeProvider = ({children}) => {
+    const [theme, setTheme] = useState ('light');
+
+    const changeTheme = () => {
+    setTheme(currentTheme => currentTheme === 'light' ? 'dark' : 'light');
+  };
+
+  const contextVal = {theme, changeTheme};
+
+  return <ThemeContext.Provider value={contextVal}>
+    {children}
+    </ThemeContext.Provider>
+}
 
 export default ThemeContext;
