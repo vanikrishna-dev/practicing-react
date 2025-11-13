f7a9cc39ca9274fb599f577a1d915c582e89e28c
vanikrishna-dev <vanikrishna02@gmail.com>
2025-11-14 01:00:34 +0530

    35th commit

--- 9-Context-API/src/components/Button.jsx
+++ 9-Context-API/src/components/Button.jsx
@@ -3,7 +3,7 @@ import ThemeContext from "../store/ThemeContext";
 
 const Button = () => {
     const {theme, changeTheme} = useContext(ThemeContext); 
-    return <center><button className={`m-3 p-3 text-xl rounded-2xl font-bold ${theme === 'light' ? "bg-pink-950 text-white" : "bg-indigo-950 text-indigo-50 border-2 border-indigo-50"} `} onClick={changeTheme}>
+    return <center><button className={`m-3 p-3 text-xl rounded-2xl font-bold ${theme === 'light' ? "bg-pink-950 text-white hover:bg-pink-200 hover:text-pink-800 hover:border-2 hover:border-pink-950" : "bg-indigo-950 text-indigo-50 border-2 border-indigo-50 hover:bg-gray-400 hover:text-indigo-950 hover:border-2 hover:border-indigo-300"} `} onClick={changeTheme}>
         Try changing the theme to {theme ==='light' ? "dark" : "light"}!
         </button></center>
 }
