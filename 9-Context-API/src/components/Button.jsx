e1d3237fc059861a752cd1b557cd9924019c9395
vanikrishna-dev <vanikrishna02@gmail.com>
2025-11-14 00:52:26 +0530

    34th commit

--- 9-Context-API/src/components/Button.jsx
+++ 9-Context-API/src/components/Button.jsx
@@ -3,9 +3,9 @@ import ThemeContext from "../store/ThemeContext";
 
 const Button = () => {
     const {theme, changeTheme} = useContext(ThemeContext); 
-    return <button className={`m-3 p-3 text-xl rounded-2xl font-bold ${theme === 'light' ? "bg-pink-950 text-white" : "bg-indigo-950 text-indigo-50 border-2 border-indigo-50"} `} onClick={changeTheme}>
-        Change Theme 
-        </button>
+    return <center><button className={`m-3 p-3 text-xl rounded-2xl font-bold ${theme === 'light' ? "bg-pink-950 text-white" : "bg-indigo-950 text-indigo-50 border-2 border-indigo-50"} `} onClick={changeTheme}>
+        Try changing the theme to {theme ==='light' ? "dark" : "light"}!
+        </button></center>
 }
 
 export default Button;
--- 9-Context-API/src/components/Heading.jsx
+++ 9-Context-API/src/components/Heading.jsx
@@ -7,7 +7,7 @@ const Heading = () => {
   console.log (theme);
 
     return <h1 className={`text-3xl p-5 text-center font-bold border-4 ${theme === 'light' ? "text-pink-950 bg-pink-300 border-pink-950" : "text-indigo-200 bg-indigo-950 border-indigo-50"} `}>
-        Welcome to Theme Changing App!
+        Welcome to the Theme Changing App!
       </h1>
 } 
 
